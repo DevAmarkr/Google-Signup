67f9567b16949313423d3969d1ed1f510053da91
DevAmarkr <amar.dev2214@gmail.com>
2019-01-08 16:44:46 +0530

    serialized and deserialized the user info

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -3,6 +3,16 @@ const GoogleStrategy = require('passport-google-oauth20')
 const key = require('./keys')
 const User = require('../models/usermodel')
 
+passport.serializeUser((user,done)=>{ // storing user id in cookie
+  done(null,user.id);//calling done method
+})
+
+passport.deserializeUser((user.id,done)=>{ //cookie give  us user id and on that basis we check into our database
+  User.findById(id).then((user)=>{  // send back the user data
+    done(null,user)
+  });
+})
+
 passport.use(new GoogleStrategy({
   //option for the GoogleStrategy
   callbackURL:'/auth/google/redirect',
@@ -14,12 +24,14 @@ passport.use(new GoogleStrategy({
   User.findOne({googleId:profile.id}).then((currentUser)=>{
     if(currentUser){
       console.log(`user is already present in database hare it his profile data ${currentUser}`)
+      done(null,currentUser)
     }else{
       new User({
         username:profile.displayName,
         googleId:profile.id
       }).save().then(newUser=>{
         console.log(`successfully user signup with ${newUser}`)
+        done(null,newUser)
       })
     }
   })
